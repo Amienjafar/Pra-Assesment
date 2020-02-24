5303cc98e3d1cb3dadf436c0d9c632a431703d90
Amienjafar <amienjafar120898@gmail.com>
2020-02-24 11:36:08 +0700

    firt

--- app/src/main/java/org/d3if0127/pra_assesment/MainActivity.kt
+++ app/src/main/java/org/d3if0127/pra_assesment/MainActivity.kt
@@ -2,11 +2,24 @@ package org.d3if0127.pra_assesment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
